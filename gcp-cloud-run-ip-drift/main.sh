264fd9d43748fe73c8110479dfdfce7d29fe9bb6
K Rin <rin@sandb0x.tw>
2024-11-25 06:24:32 +0000

    Enhance IP drifting performance.

--- gcp-cloud-run-ip-drift/main.sh
+++ gcp-cloud-run-ip-drift/main.sh
@@ -1,6 +1,15 @@
-gcloud compute routers nats update default-router   --router=default-nat   --region=asia-east1   --nat-external-ip-pool=external-ip-2   --nat-all-subnet-ip-ranges   --enable-endpoint-independent-mapping
-gcloud compute addresses delete external-ip-1 --region=asia-east1
-gcloud compute addresses create external-ip-1 --region=asia-east1
-gcloud compute routers nats update default-router   --router=default-nat   --region=asia-east1   --nat-external-ip-pool=external-ip-1   --nat-all-subnet-ip-ranges   --enable-endpoint-independent-mapping
-gcloud compute addresses delete external-ip-2 --region=asia-east1
-gcloud compute addresses create external-ip-2 --region=asia-east1
+while :; do
+    regions=("asia-east1" "asia-east2" "asia-south1")
+    {
+        for region in "${regions[@]}"; do
+            echo $region
+            gcloud compute addresses create external-ip-2 --region=$region
+            gcloud compute routers nats update default-router   --router=default-nat   --region=$region   --nat-external-ip-pool=external-ip-2   --nat-all-subnet-ip-ranges   --enable-endpoint-independent-mapping
+            gcloud compute addresses delete external-ip-1 --region=$region --quiet
+            gcloud compute addresses create external-ip-1 --region=$region
+            gcloud compute routers nats update default-router   --router=default-nat   --region=$region   --nat-external-ip-pool=external-ip-1   --nat-all-subnet-ip-ranges   --enable-endpoint-independent-mapping
+            gcloud compute addresses delete external-ip-2 --region=$region --quiet
+        done
+    } &
+    sleep 60
+done
